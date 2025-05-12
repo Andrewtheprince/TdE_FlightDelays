0abc1f63c2ddd487d6c7361738562b857effe383
Andrewtheprince <andry.mazzo@outlook.com>
2025-05-12 10:00:52 +0200

    first commit

--- UI/view.py
+++ UI/view.py
@@ -23,21 +23,8 @@ class View(ft.UserControl):
         self._title = ft.Text("Hello World", color="blue", size=24)
         self._page.controls.append(self._title)
 
-        #ROW with some controls
-        # text field for the name
-        self.txt_name = ft.TextField(
-            label="name",
-            width=200,
-            hint_text="Insert a your name"
-        )
 
-        # button for the "hello" reply
-        self.btn_hello = ft.ElevatedButton(text="Hello", on_click=self._controller.handle_hello)
-        row1 = ft.Row([self.txt_name, self.btn_hello],
-                      alignment=ft.MainAxisAlignment.CENTER)
-        self._page.controls.append(row1)
 
-        # List View where the reply is printed
         self.txt_result = ft.ListView(expand=1, spacing=10, padding=20, auto_scroll=True)
         self._page.controls.append(self.txt_result)
         self._page.update()
